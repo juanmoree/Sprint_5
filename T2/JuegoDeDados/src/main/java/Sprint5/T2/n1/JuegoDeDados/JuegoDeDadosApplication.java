925f1cfa03b2a8649e6018a992b71315edc1a1fb
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2024-01-05 18:47:54 +0100

    Project config

---
+++ T2/JuegoDeDados/src/main/java/Sprint5/T2/n1/JuegoDeDados/JuegoDeDadosApplication.java
@@ -0,0 +1,13 @@
+package Sprint5.T2.n1.JuegoDeDados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuegoDeDadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuegoDeDadosApplication.class, args);
+	}
+
+}
