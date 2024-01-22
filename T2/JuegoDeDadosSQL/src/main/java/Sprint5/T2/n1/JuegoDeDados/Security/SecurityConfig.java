2c39c5ffab62698e992d0506fa1c745ca5c99080
Juanmoree <105069930+juanmoree@users.noreply.github.com>
2024-01-22 23:22:30 +0100

    Added Jwt Utils

--- T2/JuegoDeDadosSQL/src/main/java/Sprint5/T2/n1/JuegoDeDados/Security/SecurityConfig.java
+++ T2/JuegoDeDadosSQL/src/main/java/Sprint5/T2/n1/JuegoDeDados/Security/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -45,7 +45,7 @@ public class SecurityConfig {
 
     @Bean
     PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
     // Admin autenticacion usuarios
